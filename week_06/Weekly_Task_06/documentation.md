2471932d582d3d03e5a980e7892197993e15f6a6
Leanne <l.gonzales0120241@arts.ac.uk>
2026-02-03 21:04:08 +0000

    Added photos to documentation

---
+++ week_06/Weekly_Task_06/documentation.md
@@ -0,0 +1,15 @@
+## Weekly Task 06 Documentation
+
+## Stage 1
+![alt text](batik1-1.png)
+
+## Stage 2
+![alt text](batik2.png)
+
+## Stage 3
+![alt text](batik3.png)
+
+## Stage 4
+https://drive.google.com/file/d/13fiGoKdrNQIhGHBSFE0BRpo5u2uQgY19/view?usp=sharing
+
+## Reflection
--- week_06/Weekly_Task_06/script.js
+++ week_06/Weekly_Task_06/script.js
@@ -1,10 +1,8 @@
 const timeEl = document.getElementById("time");
 const container = document.getElementById("tiles-container");
 
-// Colors for tiles
 const colors = ["#D32F2F","#F57C00","#FFEB3B","#388E3C","#1976D2","#7B1FA2","#F06292","#009688"];
 
-// Create 24 tiles
 for (let i = 0; i < 24; i++) {
   const tile = document.createElement("div");
   tile.classList.add("tile");
@@ -12,24 +10,29 @@ for (let i = 0; i < 24; i++) {
   container.appendChild(tile);
 }
 
-// Simulate hours passing quickly for demo
 let fakeHour = 0;
+
 function updateClockAndTiles() {
   const now = new Date();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
+  
   timeEl.textContent = `${fakeHour.toString().padStart(2,'0')}:${minutes.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')}`;
 
+  
   const tiles = document.querySelectorAll(".tile");
   tiles.forEach((tile, index) => {
-    if (index < fakeHour) tile.classList.add("show");
+    if (index < fakeHour) {
+      tile.classList.add("show");
+    } else {
+      tile.classList.remove("show");
+    }
   });
 
   fakeHour++;
   if (fakeHour > 24) fakeHour = 0;
 }
 
-// Update every 500ms
 setInterval(updateClockAndTiles, 500);
 updateClockAndTiles();
